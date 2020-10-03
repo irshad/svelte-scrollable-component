12d987288de99da05b2034e0a1fa539e639fa6cb
Irshad Ali <irshadali786143@gmail.com>
2020-10-04 00:28:19 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Horizontal scrollable cards in svelte js
+An simple custom components built top on the svelte js
+
+## Example
+[Demo](docs/index.html)
+
+## Installation
+
+```
+npm i svelte-scrollable-card --save
+```
+
+## Over CDN
+
+```
+https://unpkg.com/svelte-scrollable-card@1.0.3/index.js
+```
---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import svelte from 'rollup-plugin-svelte';
+import pkg from './package.json';
+import scss from 'rollup-plugin-scss';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import { terser } from 'rollup-plugin-terser';
+import livereload from 'rollup-plugin-livereload';
+
+const production = !process.env.ROLLUP_WATCH;
+
+const name = pkg.name
+	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+	.replace(/^\w/, m => m.toUpperCase())
+	.replace(/-\w/g, m => m[1].toUpperCase());
+
+export default {
+	input: 'src/index.html',
+	output: [
+		{ file: pkg.module, 'format': 'es' },
+		{ file: pkg.main, 'format': 'umd', name }
+	],
+	plugins: [
+		svelte({
+			customElement: 'ok-horizontal-scrollbar',
+		}),
+		resolve(),
+		commonjs(),
+		production && terser(),
+	]
+};
